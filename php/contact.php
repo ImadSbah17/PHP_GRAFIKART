da2d35e1ea401e0c12b379ab7d1fd6201218374f
ImadSbah17- <imadsbah@gmail.com>
2021-05-28 12:45:39 +0200

    INTRO PHP&HTML

---
+++ php/contact.php
@@ -0,0 +1,11 @@
+<?php 
+$title = "Nous contacter";
+$nav = "contact";
+require "../php/header.php";?>
+
+<h2>Nous contacter</h2>
+
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis eaque ullam ipsa id temporibus quos laudantium animi. Alias ipsam eum, laudantium iure, in eos ipsa ipsum, ratione at facere velit!
+Sint beatae ut adipisci repudiandae illo quisquam saepe, libero, tempora doloremque nihil, harum id consectetur debitis dolor! Asperiores libero earum perspiciatis, consectetur eius quia quisquam esse, eaque beatae et ipsam!</p>
+
+<?php require "../php/footer.php"?>
---
+++ php/index.php
@@ -0,0 +1,18 @@
+<?php
+$title = "Page d'acceuil";
+require '../php/header.php';
+?>
+
+<pre>
+  <?php print_r($_SERVER);?>
+</pre>
+
+  <div class="starter-template">
+    <h1>Bootstrap starter template</h1>
+    <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+  </div>
+
+<?php require '../php/footer.php' ?>
+
+
+
